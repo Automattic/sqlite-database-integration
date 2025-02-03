d2be9d8dd233e5ab2e860663d00c730ea13b4289
Jan Jakes <jan@jakes.pro>
2025-02-03 15:44:36 +0100

    Use getters instead of public properties

--- tests/tools/dump-sqlite-query.php
+++ tests/tools/dump-sqlite-query.php
@@ -20,7 +20,7 @@ $query = "SELECT * FROM t1 LEFT JOIN t2 ON t1.id = t2.t1_id WHERE t1.name = 'abc
 
 $driver->query( $query );
 
-$executed_queries = $driver->executed_sqlite_queries;
+$executed_queries = $driver->get_executed_sqlite_queries();
 if ( count( $executed_queries ) > 2 ) {
 	// Remove BEGIN and COMMIT/ROLLBACK queries.
 	$executed_queries = array_values( array_slice( $executed_queries, 1, -1, true ) );
